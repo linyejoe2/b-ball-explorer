96caa48c436e1620fdbbf66ba4461fdd1109bd7c
Randy Lin <linyejoe2@gmail.com>
2023-04-21 14:01:08 +0800

    new feature, ui fix
    + add locate widgets, can locate user's location, help user search the b-ball court near by.
    + update popup ui to more pretty
    +  add b-ball court picture to popup

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ require([
   "esri/layers/GraphicsLayer",
   "esri/widgets/BasemapToggle",
   "esri/widgets/Popup",
-  "esri/widgets/Home"
-], function (Map, MapView, Graphic, GraphicsLayer, BasemapToggle, Popup, Home) {
+  "esri/widgets/Home",
+  "esri/widgets/Locate",
+  "esri/widgets/Search"
+], function (Map, MapView, Graphic, GraphicsLayer, BasemapToggle, Popup, Home, Locate, Search) {
 
   // 建立地圖
   var map = new Map({
@@ -53,6 +55,12 @@ require([
   var graphicsLayer = new GraphicsLayer();
   map.add(graphicsLayer);
 
+  // 顯示載入中提示
+  const loadingDiv = document.createElement("div");
+  loadingDiv.textContent = "載入中...";
+  loadingDiv.className = "loading"
+  document.body.appendChild(loadingDiv);
+
   // // 呼叫 api 取得籃球場資料，並將資料轉成 Graphics 顯示在地圖上
   fetch("https://script.google.com/macros/s/AKfycbz8dfD4bGT6NYSls3LOfsr6Gz3b3Fm3iJUyI8o_Z-axrSsQtyFhXASHoz3GZU0XkklgPw/exec")
     .then(response => response.json())
@@ -74,16 +82,85 @@ require([
           },
           popupTemplate: {
             title: feature.name,
-            content: "<b>地址：</b>" + feature.address + "<br><b>主場隊伍：</b>" + feature.team + "<br>" +
-              "<b>所屬聯盟：</b>" + feature.league + "<br>" +
-              "<b>是否對外開放：</b>" + feature.rent_state + "<br>" +
-              "<b>聯絡該場館：</b>" + feature.tel
-            ,
-          }
+            content: [
+              {
+                type: "media",
+                mediaInfos: [
+                  {
+                    type: "image",
+                    value: {
+                      sourceURL: feature.photo1,
+                      linkURL: feature.photo1,
+                    },
+                  },
+                ],
+              },
+              {
+                type: "fields",
+                fieldInfos: [
+                  {
+                    fieldName: "address",
+                    label: "地址",
+                  },
+                  {
+                    fieldName: "team",
+                    label: "主場隊伍",
+                  },
+                  {
+                    fieldName: "league",
+                    label: "所屬聯盟",
+                  },
+                  {
+                    fieldName: "rentState",
+                    label: "是否對外開放",
+                  },
+                  {
+                    fieldName: "tel",
+                    label: "聯絡該場館",
+                  },
+                ],
+              },
+            ],
+            //   content:
+            //     `
+            //   <b>地址：</b> ${feature.address} <br>
+            //   <b>主場隊伍：</b> ${feature.team} <br>
+            //   <b>所屬聯盟：</b> ${feature.league} <br>
+            //   <b>是否對外開放：</b> ${feature.rent_state} <br>
+            //   <b>聯絡該場館：</b> ${feature.tel} <br>
+            //   <img src="${feature.photo1}" alt="圖片">
+            // `,
+          },
         });
         graphicsLayer.add(graphic);
+
+        // 移除載入中提示
+        loadingDiv.remove();
       });
     });
+
+  // // 創建一個 Search widget
+  // const searchWidget = new Search({
+  //   view: view,
+  //   allPlaceholder: "搜尋籃球場名稱",
+  //   includeDefaultSources: false,
+  //   sources: [{
+  //     layer: graphicsLayer,
+  //     placeholder: "搜尋籃球場名稱",
+  //     maxResults: 5,
+  //     searchFields: ["name"],
+  //     displayField: "name",
+  //     name: "搜尋籃球場名稱",
+  //     // filter: searchExtent
+  //   }],
+  //   locationEnabled: false
+  // });
+  // view.ui.add(searchWidget, {
+  //   position: "top-right",
+  //   index: 2
+  // });
+
+
 
   // 監聽點擊事件，彈出 Popup 顯示籃球場資訊
   view.on("click", function (event) {
@@ -103,5 +180,14 @@ require([
       }
     });
   });
+
+  // 定位使用者位置
+  view.ui.add(new Locate({
+    view: view,   // Attaches the Locate button to the view
+    graphic: new Graphic({
+      symbol: { type: "simple-marker" }  // overwrites the default symbol used for the
+      // graphic placed at the location of the user when found
+    })
+  }), { position: "top-left", index: 1 });
 
 });
